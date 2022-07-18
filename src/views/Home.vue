f10214dff1126d748f953f08d29f2cdb35328a16
韩洋 <731314840@qq.com>
2022-07-18 20:48:53 +0800

    'create:定义主题色,定义边框、圆角、禁用效果'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <HyButton>默认按钮</HyButton>
     <HyButton type="primary">成功按钮</HyButton>
     <HyButton type="danger">危险按钮</HyButton>
-    <HyButton type="warning">警告按钮</HyButton>
+    <!-- <HyButton type="warning">警告按钮</HyButton> -->
   </div>
 </template>
 
